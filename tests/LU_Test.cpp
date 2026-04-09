1cdf77c1ee6481babdaed6e889beff556ac3b0e2
web-dev137 <daniyar.tuletsov.1994@mail.ru>
2026-04-09 23:56:56 +0700

    finished PA=LU

--- tests/LU_Test.cpp
+++ tests/LU_Test.cpp
@@ -1,35 +1,54 @@
 #include <gtest/gtest.h>
-#include "../include/linear-algebra/LU/LU.hpp"
+#include "../include/linear-algebra/LU/DecomposeLU.hpp"
 
 class LU_Test : public ::testing::Test
 {
 protected:
     VectorMatrix<double> A;
-    std::unique_ptr<LU<double>> lu;
+    std::unique_ptr<DecomposeLU<double>> lu;
     void SetUp() override {
         A = VectorMatrix<double>({
-            {2.0, 1.0, 1.0},
-            {4.0, -6.0, 0.0},
-            {-2.0, 7.0, 2.0}
+            {0, 2, 1},
+            {1, 1, 0},
+            {2, 1, 1}
         });
-        lu = std::make_unique<LU<double>>(A);
+        lu = std::make_unique<DecomposeLU<double>>(A);
+    }
+
+    VectorMatrix<double> reconstructA() {
+ 
+        auto P = lu->getP();
+    
+        int rows = A.getRows();
+        VectorMatrix<double> PA = A;
+        for (int i = 0; i < rows; i++)
+            PA[i] = A[P[i]];   // обратная перестановка
+        
+        return PA;
     }
 };
 
 TEST_F(LU_Test,decomposition) {
-    lu->decomposition();
-    std::vector<std::vector<double>> E;
-    E = std::vector<std::vector<double>>({
-        {4.0, -6.0, 0.0},
-        {0.0, 4.0, 1.0},
-        {0.0, 0.0, 1.0}
-    });
-    
-    auto U = lu->getU();
 
-    for (size_t i = 0; i < E.size(); i++) {
-        for (size_t j = 0; j < E[i].size(); j++) {
-            EXPECT_DOUBLE_EQ(E[i][j], U[i][j]);
+    VectorMatrix<double> PA = reconstructA();
+    auto U = lu->getU();
+    auto L = lu->getL();
+    VectorMatrix<double> vU(U);
+    VectorMatrix<double> vL(L);
+    VectorMatrix<double> LU = vL*vU;
+    for (int i = 0; i < PA.getRows(); i++)
+    {
+        for (int j = 0; j < PA.getColumns(); j++)
+        {
+            EXPECT_DOUBLE_EQ(PA[i][j],LU[i][j]);
         }
+        
     }
+    
+}
+
+TEST_F(LU_Test, det) {
+    double det = lu->det();
+
+    EXPECT_DOUBLE_EQ(-3,det);
 }
