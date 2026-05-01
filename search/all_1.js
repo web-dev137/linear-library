779cc577883ae44bc3d7a35e6c8b939b8fbf0caf
web-dev137 <web-dev137@users.noreply.github.com>
2026-05-01 17:22:11 +0000

    deploy: 650d175681ff8c78c74fa047823eb01585e0c9b5

--- search/all_1.js
+++ search/all_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['getcolumns_0',['getColumns',['../classLinearAlgebra_1_1VectorMatrix.html#aaec1d666d37ef84af62e3bf58e4053f7',1,'LinearAlgebra::VectorMatrix']]],
   ['getl_1',['getL',['../classLinearAlgebra_1_1DecomposeLU.html#afac4fddba4f032968b533f90ccf7f173',1,'LinearAlgebra::DecomposeLU']]],
-  ['getmatrix_2',['getMatrix',['../classLinearAlgebra_1_1VectorMatrix.html#a8798dd92296528c84ba6a0e53f324f3a',1,'LinearAlgebra::VectorMatrix']]],
+  ['getmatrix_2',['getmatrix',['../classLinearAlgebra_1_1VectorMatrix.html#a8798dd92296528c84ba6a0e53f324f3a',1,'LinearAlgebra::VectorMatrix::getMatrix() const'],['../classLinearAlgebra_1_1VectorMatrix.html#ab5cf1a34948aef7b2d334e8752e48a6f',1,'LinearAlgebra::VectorMatrix::getMatrix()']]],
   ['getp_3',['getP',['../classLinearAlgebra_1_1DecomposeLU.html#a09bc124d3621f2141763848c69fda074',1,'LinearAlgebra::DecomposeLU']]],
   ['getrows_4',['getRows',['../classLinearAlgebra_1_1VectorMatrix.html#ad6d424ab8c81a123d64053595c47e5f9',1,'LinearAlgebra::VectorMatrix']]],
   ['getu_5',['getU',['../classLinearAlgebra_1_1DecomposeLU.html#af90d67c49e1fd99cac60c50da3eda842',1,'LinearAlgebra::DecomposeLU']]]
