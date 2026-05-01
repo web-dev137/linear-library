42407dcf48578dcf90672502292b1745c7fb4c76
web-dev137 <web-dev137@users.noreply.github.com>
2026-05-01 17:50:11 +0000

    deploy: 94c2457450910640184b852797160722ca297f60

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['decomposelu_0',['decomposelu',['../classLinearAlgebra_1_1DecomposeLU.html#a5f35f6d9c19f75323df8cacd508b0228',1,'LinearAlgebra::DecomposeLU::DecomposeLU(const VectorMatrix&lt; T &gt; &amp;m)'],['../classLinearAlgebra_1_1DecomposeLU.html#aa20a1a3e145630c9e666e3a925aebec9',1,'LinearAlgebra::DecomposeLU::DecomposeLU(VectorMatrix&lt; T &gt; &amp;&amp;m)']]],
-  ['det_1',['det',['../classLinearAlgebra_1_1DecomposeLU.html#a465793ac252c5d284578361fa7cea362',1,'LinearAlgebra::DecomposeLU']]]
+  ['det_1',['det',['../classLinearAlgebra_1_1DecomposeLU.html#ac2484d99bea094ab5ba28ac4c982af70',1,'LinearAlgebra::DecomposeLU']]]
 ];
