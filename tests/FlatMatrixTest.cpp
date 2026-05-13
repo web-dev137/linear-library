501e4b072b4ed2f09103590024c53be6e2daa80f
web-dev137 <daniyar.tuletsov.1994@mail.ru>
2026-05-13 18:58:47 +0700

    FlatMatrix class:multiply(block multiply)|multiply on scalar|add|fix assert in ()|tests

--- tests/FlatMatrixTest.cpp
+++ tests/FlatMatrixTest.cpp
@@ -6,12 +6,50 @@ using namespace LinearAlgebra;
 class FlatMatrixTest : public ::testing::Test
 {
 protected:
-   
+    FlatMatrix<int> A;
+    FlatMatrix<int> B;
+       void SetUp() override{
+        A ={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        B = {
+            {10,11,12},
+            {13,14,15},
+            {16,17,18}
+        };
+    }
 };
 
-TEST(FlatMatrixTest,operatorGet) {
+TEST_F(FlatMatrixTest,operatorGet) {
+    int val = A(0,2);
+    EXPECT_EQ(val,3);
+}
+
+TEST_F(FlatMatrixTest, operatorMultiply) {
+
+    FlatMatrix<int> Exp({
+        {84,  90,  96},
+        {201, 216, 231},
+        {318, 342, 366}
+    });
+    const auto& C = A*B;
+    int rows = A.getRows();
+    int cols = B.getCols();
+    
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            EXPECT_NEAR(C(i,j),Exp(i,j),1e-9);
+        }
+    }
+}
+
+TEST(FlatMatrixTest,operatorOstream) {
     std::vector<int> v = {1, 2, 3, 4, 5, 6};
     FlatMatrix<int> A(2,3,v);
-    double val = A(0,2);
-    EXPECT_DOUBLE_EQ(val,3.0);
+    std::ostringstream oss;
+    oss << A;
+    EXPECT_EQ(oss.str(),"1 2 3 \n4 5 6\n");
 }
